6ba4f06e47e56b2cb092e5bed5cbfbce3ee6829c
vlad <v>
2021-01-31 01:59:54 +0300

    renaming configs

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/pvelx/trigger-service-demo.git ../trigger-service-demo
+git clone https://github.com/pvelx/message-dashboard-demo.git ../message-dashboard-demo
+git clone https://github.com/pvelx/message-service-demo.git ../message-service-demo
+
